9cd88fcf0b8d537cc91080f2ddd61c90f5b649ed
Kiru6ik <116097426+Kiru6ik@users.noreply.github.com>
2023-08-25 15:28:19 -0400

    Update linkedin_logging_in.py
    add comments

--- linkedin_logging_in.py
+++ linkedin_logging_in.py
@@ -1,21 +1,21 @@
-from linkedin_scraper import Person, actions
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+# Getting necessary tools for our program.
 
+from linkedin_scraper import Person, actions  # Tools for fetching LinkedIn data.
+from selenium.webdriver.chrome.options import Options  # Allows us to set special settings for our browser.
+from selenium import webdriver  # Tool that lets our program interact with a web browser.
+from selenium.webdriver.chrome.service import Service as ChromeService  # Helps manage our Chrome browser.
+from webdriver_manager.chrome import ChromeDriverManager  # Makes sure we get the latest version of the Chrome browser.
+
+# Setting up specific settings for our browser.
 options = Options()
-options.add_argument("user-data-dir=C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 3")
+options.add_argument("user-data-dir=C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 3")  # Pointing to a specific browser profile.
 
-# username = "sashopigas@gmail.com"
-# password = "Sasho1234"
-
+# Starting our web browser with the settings we've defined.
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
+# Pausing our script for a while.
 import time
 
-username = "sashopigas@gmail.com"
-password = "Sasho1234"
-
+# Using our browser to visit LinkedIn's main page.
 driver.get("https://www.linkedin.com/")
-time.sleep(300)
+time.sleep(300)  # Pausing for 300 seconds, so you have time to interact with the page, login or do some tasks.
